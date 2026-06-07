ff6118feefd852719ed0074895dd12ccb6880db2
Irnitee Patel <256488812+irnitee-patel@users.noreply.github.com>
2026-06-07 23:17:32 +0530

    Enhance save_report to include incoming and outgoing counts

--- utils.py
+++ utils.py
@@ -4,14 +4,41 @@ def save_report(
         video_name,
         total_detected,
         total_counted,
+        incoming_count,
+        outgoing_count,
         processing_time,
         output_path):
 
+    vehicles_per_minute = 0
+
+    if processing_time > 0:
+
+        vehicles_per_minute = (
+            total_counted /
+            processing_time
+        ) * 60
+
     report = {
+
         "video_name": video_name,
-        "total_vehicles_detected": total_detected,
-        "total_vehicles_counted": total_counted,
-        "processing_time_seconds": round(processing_time, 2)
+
+        "total_vehicles_detected":
+            total_detected,
+
+        "total_vehicles_counted":
+            total_counted,
+
+        "incoming_vehicles":
+            incoming_count,
+
+        "outgoing_vehicles":
+            outgoing_count,
+
+        "vehicles_per_minute":
+            round(vehicles_per_minute, 2),
+
+        "processing_time_seconds":
+            round(processing_time, 2)
     }
 
     with open(output_path, "w") as file:
